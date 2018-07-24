3141297c8dd1ed6866107c12895d0aa3831f12ce
chuping.cui <chuping.cui@okcoin.com>
2018-07-24 17:38:56 +0800

    white component

--- src/components/white/WhitePreview.vue
+++ src/components/white/WhitePreview.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="title" :style="{ height: data.height + 'px' }"></div>
+  <div class="white" :style="{ height: data.height + 'px' }"></div>
 </template>
 <script>
 export default {
@@ -10,7 +10,7 @@ export default {
 }
 </script>
 <style scoped>
-.title {
+.white {
   background-color: #eff2f7;
 }
 </style>
